f153b940a7ee6ecceea66aba03ff99926d95e8b4
Silvio Machado <silvio.machado@cwi.com.br>
2018-02-28 10:48:54 -0300

    Added procfile

--- myApp/mal/models.py
+++ myApp/mal/models.py
@@ -1,18 +1,28 @@
 from django.db import models
 
 class Topic(models.Model):
-    top_name = models.CharField(max_length = 264, unique=True)
+    top_name = models.CharField(max_length=264, unique=True)
 
     def __str__(self):
         return self.top_name
 
 class Animes (models.Model):
+    JA_ASSISTI = "ja"
+    QUERO_ASSISTIR = "qa"
+    ESTOU_ASSISTINDO = "ea"
+    STATUS_CHOICES = [
+        (JA_ASSISTI, "ja assisti"),
+        (QUERO_ASSISTIR, "Quero assistir"),
+        (ESTOU_ASSISTINDO, "Estou assistindo"),
+    ]
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
-    name = models.CharField(max_length = 264, unique=True)
-    
+    name = models.CharField(max_length=264, unique=True)
+    status = models.CharField(max_length =264, choices=STATUS_CHOICES, default=QUERO_ASSISTIR)
     class Meta:
         verbose_name = 'Anime'
         verbose_name_plural = 'Animes'
 
+        
     def __str__(self):
         return self.name
+
