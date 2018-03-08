96a127560e03e870d6119bd93345926574645667
Silvio Machado <silvio.machado@cwi.com.br>
2018-03-08 16:15:38 -0300

    aded new models field.

--- myApp/mal/models.py
+++ myApp/mal/models.py
@@ -1,10 +1,14 @@
+import datetime
+from django.utils import timezone
 from django.db import models
+
 
 class Topic(models.Model):
     top_name = models.CharField(max_length=264, unique=True)
-
+        
     def __str__(self):
         return self.top_name
+
 
 class Animes (models.Model):
     JA_ASSISTI = "ja"
@@ -16,9 +20,11 @@ class Animes (models.Model):
         (ESTOU_ASSISTINDO, "Estou assistindo"),
     ]
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
-    name = models.CharField(max_length=264, unique=True)
+    name = models.CharField(max_length=264)
     status = models.CharField(max_length =264, choices=STATUS_CHOICES, default=QUERO_ASSISTIR)
+    pub_date = models.DateTimeField('date published', default=timezone.now)
     class Meta:
+        unique_together = ('topic', 'name')
         verbose_name = 'Anime'
         verbose_name_plural = 'Animes'
 
@@ -26,3 +32,5 @@ class Animes (models.Model):
     def __str__(self):
         return self.name
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
